501bc2f44679a8c184e94ca999e89a49bd76a561
Farhan <112369332+Flash531@users.noreply.github.com>
2025-06-14 19:23:55 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
-   <BrowserRouter>
-      <StoreContextProvider>
-         <App />
-      </StoreContextProvider>
-   </BrowserRouter>
+   <StrictMode>
+      <BrowserRouter>
+         <StoreContextProvider>
+            <App />
+            <ToastContainer position="top-center" autoClose={3000} />
+         </StoreContextProvider>
+      </BrowserRouter>
+   </StrictMode>
 );
