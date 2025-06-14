8decc1d014ad910472a7bf589e2ef397891899fb
Farhan <112369332+Flash531@users.noreply.github.com>
2025-06-14 16:44:04 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
 import Navbar from './components/Navbar/Navbar';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import List from './pages/List/List';
 import Orders from './pages/Orders/Orders';
 import Add from './pages/Add/Add';
@@ -9,25 +9,30 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-
-   const url = "http://localhost:4000";
+  const url = "http://localhost:4000";
 
   return (
     <div>
-      <ToastContainer/>
-      <Navbar/>
+      <ToastContainer />
+      <Navbar />
       <hr />
       <div className="app-content">
-        <Sidebar/>
+        <Sidebar />
         <Routes>
-          <Route path = "/add" element={<Add url={url}/>}/>
-          <Route path = "/list" element={<List url={url}/>}/>
-          <Route path = "/orders" element={<Orders url={url}/>}/>
+          {/* Redirect root "/" to "/orders" */}
+          <Route path="/" element={<Navigate to="/orders" />} />
+
+          {/* Defined routes */}
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+
+          {/* Catch-all route for unmatched URLs */}
+          <Route path="*" element={<h2 style={{ margin: '20px' }}>404 - Page Not Found</h2>} />
         </Routes>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
